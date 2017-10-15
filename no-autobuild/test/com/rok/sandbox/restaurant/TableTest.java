344517ba4ad9ab50d3324a748b7431706e3d6dec
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2017-10-15 23:50:28 +0300

    Add simple project without automation build tool

---
+++ no-autobuild/test/com/rok/sandbox/restaurant/TableTest.java
@@ -0,0 +1,34 @@
+package com.rok.sandbox.restaurant;
+
+import com.rok.sandbox.restaurant.model.Table;
+import com.rok.sandbox.restaurant.model.Visitor;
+import junit.framework.TestCase;
+
+public class TableTest extends TestCase {
+	
+	public void testTable(){
+		Table b1 = new Table(1, "table for 4");
+		assertEquals("table for 4", b1.getDescription());
+		assertEquals(1, b1.getNumber());
+	}
+	
+	public void testGetVisitor(){
+		Table b2 = new Table(2, "Table for three");
+		Visitor p2 = new Visitor("Elvis");
+
+		b2.setVisitor(p2);
+		
+		String testName = b2.getVisitor().getName();
+		assertEquals("Elvis", testName);
+	}
+	
+	public void testToString(){
+		Table b2 = new Table(3, "Table for three");
+		Visitor p2 = new Visitor("Elvis");
+
+		assertEquals("Table # 3 (table for three); Free", b2.toString());
+		b2.setVisitor(p2);
+		assertEquals("Table # 3 (table for three); Booked by Elvis", b2.toString());
+	}
+	
+}
